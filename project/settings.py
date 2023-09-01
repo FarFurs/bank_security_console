658a0d4fe00551bc598835dfa4893430411fada7
Mike Gaev <mikegaev@MacBook-Air-Mike.local>
2023-09-01 17:21:55 +0300

    Added ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -1,31 +1,35 @@
 import os
 from dotenv import load_dotenv
+from environs import Env
 
 
 dotenv_path = os.path.join(os.path.split(os.path.dirname(__file__))[0], '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+    env = Env()
+    env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ["DB_ENGINE"],
-        'HOST': os.environ["DB_HOST"],
-        'PORT': os.environ["DB_PORT"],
-        'NAME': os.environ["DB_NAME"],
-        'USER': os.environ["DB_USER"],
-        'PASSWORD': os.environ["DB_PASSWORD"],
+        'ENGINE': env("DB_ENGINE"),
+        'HOST': env("DB_HOST"),,
+        'PORT': env("DB_PORT"),,
+        'NAME': env("DB_NAME"),,
+        'USER': env("DB_USER"),,
+        'PASSWORD': env("DB_PASSWORD"),,
     }
 }
 
+
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = False
+DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
